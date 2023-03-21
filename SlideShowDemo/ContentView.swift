66676e4b882595c124731a5a3aa7068d450f9bdc
John Henry Thompson <jht1493@gmail.com>
2023-03-21 10:18:01 -0400

    SlideShowDemo/AudioDJ

--- SlideShowDemo/ContentView.swift
+++ SlideShowDemo/ContentView.swift
@@ -11,24 +11,24 @@ import SwiftUI
 // could passed as EnvironmentObject
 let slides = ["fish","ant","hare","ladybug","tortoise"]
 
-struct ContentView: View {
-  var body: some View {
-    NavigationView {
-      VStack {
-        NavigationLink (destination: NextPreviousView()) {
-          Text("Next Previous Link")
+struct ContentView: View {    
+    var body: some View {
+        NavigationView {
+            VStack {
+                NavigationLink (destination: NextPreviousView()) {
+                    Text("Next Previous Link")
+                }
+                NavigationLink (destination: PlayPauseView()) {
+                    Text("Play Pause Link")
+                }
+                Spacer()
+            }
         }
-        NavigationLink (destination: PlayPauseView()) {
-          Text("Play Pause Link")
-        }
-        Spacer()
-      }
     }
-  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 }
--- SlideShowDemo/PlayPauseView.swift
+++ SlideShowDemo/PlayPauseView.swift
@@ -8,62 +8,71 @@
 import SwiftUI
 
 struct PlayPauseView: View {
-  @State var slideIndex = 0
-  @State var isPlaying = false
-  // Timer gets called every second.
-  let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-  var body: some View {
-    VStack {
-      Text("Play Pause Slides")
-        .font(Font.system(size: 30, weight: .bold))
-        .padding()
-      // slides is defined in NextPreviousView
-      let name = slides[slideIndex]
-      SlideView(name: name)
-      HStack {
-        Button(action: previousItemAction) {
-          Image(systemName: "chevron.left")
-            .resizable()
-            .frame(width: 40, height: 40)
+    @State var slideIndex = 0
+    @State var isPlaying = false
+    // Timer gets called every second.
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
+    @StateObject var audioDJ = AudioDJ()
+
+    var body: some View {
+        VStack {
+            Text("Play Pause Slides")
+                .font(Font.system(size: 30, weight: .bold))
+                .padding()
+            // slides is defined in NextPreviousView
+            let name = slides[slideIndex]
+            SlideView(name: name)
+            HStack {
+                Button(action: previousItemAction) {
+                    Image(systemName: "chevron.left")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                }
+                .padding()
+                Spacer()
+                Button(action: playPauseAction) {
+                    Image(systemName: isPlaying ? "pause" : "play")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                }
+                Spacer()
+                Button(action: nextItemAction) {
+                    Image(systemName: "chevron.right")
+                        .resizable()
+                        .frame(width: 40, height: 40)
+                }
+                .padding()
+            }
         }
-        .padding()
-        Spacer()
-        Button(action: playPauseAction) {
-          Image(systemName: isPlaying ? "pause" : "play")
-            .resizable()
-            .frame(width: 40, height: 40)
+        .onReceive(timer) { _ in
+            // Block gets called when timer updates.
+            if (isPlaying) {
+                nextItemAction()
+            }
         }
-        Spacer()
-        Button(action: nextItemAction) {
-          Image(systemName: "chevron.right")
-            .resizable()
-            .frame(width: 40, height: 40)
-        }
-        .padding()
-      }
     }
-    .onReceive(timer) { _ in
-      // Block gets called when timer updates.
-      if (isPlaying) {
-        nextItemAction()
-      }
+    
+    func playPauseAction() {
+        isPlaying.toggle()
+        if isPlaying {
+            audioDJ.play()
+        }
+        else {
+            audioDJ.stop()
+        }
     }
-  }
-  
-  func playPauseAction() {
-    isPlaying.toggle()
-  }
-  func previousItemAction() {
-    slideIndex = (slideIndex - 1 + slides.count) % slides.count
-  }
-  func nextItemAction() {
-    slideIndex = (slideIndex + 1) % slides.count
-  }
+    func previousItemAction() {
+        slideIndex = (slideIndex - 1 + slides.count) % slides.count
+    }
+    func nextItemAction() {
+        slideIndex = (slideIndex + 1) % slides.count
+    }
 }
 
 
 struct PlayPause_Previews: PreviewProvider {
-  static var previews: some View {
-    PlayPauseView()
-  }
+    static var previews: some View {
+        PlayPauseView()
+    }
 }
